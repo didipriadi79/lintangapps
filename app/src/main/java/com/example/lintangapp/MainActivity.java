0435a8e544ac730790807fd40cac0b6301785928
Lintang Bongkar IRIS <lintang.bb@gmail.com>
2021-12-17 16:51:08 -0800

    project

--- app/src/main/java/com/example/lintangapp/MainActivity.java
+++ app/src/main/java/com/example/lintangapp/MainActivity.java
@@ -1,25 +1,38 @@
 package com.example.lintangapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.loader.content.AsyncTaskLoader;
+import androidx.loader.content.Loader;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.lintangapp.github.GithubView;
+import com.example.lintangapp.github.NetworkUtils;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout layoutEmpty;
 
     EditText searchUser;
-    RecyclerView listUser;
+    ListView listUser;
     FloatingActionButton add;
+
+    private static final int GITHUB_SEARCH_LOADER = 1;
+    private static final String GITHUB_QUERY_TAG = "query";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,5 +63,68 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         });
+
+        if (savedInstanceState != null){
+            String queryUrl = savedInstanceState.getString(GITHUB_QUERY_TAG);
+            searchUser.setText(queryUrl);
+        }
+
+        getSupportLoaderManager().initLoader(GITHUB_SEARCH_LOADER, null, this);
+
+    }
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistanceState){
+        super.onSaveInstanceState(outState, outPersistanceState);
+        outState.putString(GITHUB_QUERY_TAG, searchUser.getText().toString().trim());
+    }
+
+    public Loader<List<GithubView>> onCreateLoader(int d, final Bundle args){
+        return new AsyncTaskLoader<List<GithubView>>(this) {
+            List<GithubView> mRepositoryList;
+
+            @Override
+            protected void onStartLoading(){
+
+                if(args == null){
+                    return;
+                }
+
+                if (mRepositoryList == null){
+                    deliverResult(mRepositoryList);
+                } else {
+                    forceLoad();
+                }
+                //super.onStartLoading();;
+            }
+
+            @Nullable
+            @Override
+            public List<GithubView> loadInBackground() {
+
+                String searchQueryUrlString = args.getString(GITHUB_QUERY_TAG);
+
+                try {
+                    List<GithubView> githubSearchResult = NetworkUtils.getDataFromApi(searchQueryUrlString);
+                    return githubSearchResult;
+                } catch (IOException e){
+                    e.printStackTrace();
+                    return null;
+                }
+
+            }
+            @Override
+            public void deliverResult(@Nullable List<GithubView> githubjson){
+                mRepositoryList = githubjson;
+                super.deliverResult(githubjson);
+            }
+        };
+    }
+
+    public void onLoadFinished(Loader<List<GithubView>> Loader, List<GithubView> data) {
+        if (data == null){
+            adapter.clear();
+            adapter.addAll(data);
+            showJsonDataView();
+        }
     }
 }
